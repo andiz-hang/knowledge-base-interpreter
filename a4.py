ae477a637154af80b37b9f15e237b81c619690c2
andiz <andiz@sfu.ca>
2020-07-26 09:51:41 -0700

    a4

--- a4.py
+++ a4.py
@@ -57,14 +57,13 @@ def printError(string):
 
 # returns True if, and only if, string s is a valid variable name
 def is_atom(s):
+    def is_letter(s):
+        return len(s) == 1 and s.lower() in "_abcdefghijklmnopqrstuvwxyz"
     if not isinstance(s, str):
         return False
     if s == "":
         return False
     return is_letter(s[0]) and all(is_letter(c) or c.isdigit() for c in s[1:])
-
-def is_letter(s):
-    return len(s) == 1 and s.lower() in "_abcdefghijklmnopqrstuvwxyz"
 
 def interpret_file(kbString):
     kb_dict = {}
